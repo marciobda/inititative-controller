b77b732373f072756d6d2657d58c41539b4485ca
marciobda <marciobda@gmail.com>
2016-01-29 01:11:12 -0200

    touch correction and general file organization

--- app/js/app.js
+++ app/js/app.js
@@ -2,60 +2,60 @@
 var app = angular.module('Initiative',[]);
 
 /**
- * Initiative Controller
- *
- * Manage the initiative list operations
- */
+* Initiative Controller
+*
+* Manage the initiative list operations
+*/
 app.controller('InitiativeCtrl',function($scope){
 
-    // Base list
-    $scope.actors = [
-        { type : 'player',  name : '', initValue : 10 },
-        { type : 'player',  name : '', initValue : 10 },
-        { type : 'player',  name : '', initValue : 10 },
-        { type : 'monster', name : '',  initValue : 10 }
-    ];
+  // Base list
+  $scope.actors = [
+    { type : 'player',  name : '', initValue : 10 },
+    { type : 'player',  name : '', initValue : 10 },
+    { type : 'player',  name : '', initValue : 10 },
+    { type : 'monster', name : '',  initValue : 10 }
+  ];
 
-    // Replace the order of two list items
-    $scope.reorder = function (initial, target) {
-        var temp = $scope.actors[target];
-        $scope.actors[target] = $scope.actors[initial];
-        $scope.actors[initial] = temp;
-        $scope.$apply();
-    };
+  // Replace the order of two list items
+  $scope.reorder = function (initial, target) {
+    var temp = $scope.actors[target];
+    $scope.actors[target] = $scope.actors[initial];
+    $scope.actors[initial] = temp;
+    $scope.$apply();
+  };
 
-    $scope.insert = function (initial, target) {
-        var temp = $scope.actors[initial];
-        $scope.actors.splice(initial,1);
-        $scope.actors.splice(target,0,temp);
-        $scope.$apply();
-    };
+  $scope.insert = function (initial, target) {
+    var temp = $scope.actors[initial];
+    $scope.actors.splice(initial,1);
+    $scope.actors.splice(target,0,temp);
+    $scope.$apply();
+  };
 
-    $scope.addActor = function(type) {
-        if(type === 'player') {
-            $scope.actors.push({ type : 'player',  name : '', initValue : 10  });
-        } else if(type === 'monster') {
-            $scope.actors.push({ type : 'monster',  name : '', initValue : 10  });
-        }
-    };
-
-    $scope.calcInitiative = function() {
-        var initCompare = function (a, b) {
-            if (a.initValue > b.initValue) {
-                return -1;
-            }
-            if (a.initValue < b.initValue) {
-                return 1;
-            }
-            return 0;
-        }
-        $scope.actors.sort(initCompare);
-        //$scope.$apply();
+  $scope.addActor = function(type) {
+    if(type === 'player') {
+      $scope.actors.push({ type : 'player',  name : '', initValue : 10  });
+    } else if(type === 'monster') {
+      $scope.actors.push({ type : 'monster',  name : '', initValue : 10  });
     }
+  };
 
-    $scope.remove = function(index) {
-        $scope.actors.splice(index,1);
-        $scope.$apply();
+  $scope.calcInitiative = function() {
+    var initCompare = function (a, b) {
+      if (a.initValue > b.initValue) {
+        return -1;
+      }
+      if (a.initValue < b.initValue) {
+        return 1;
+      }
+      return 0;
     }
+    $scope.actors.sort(initCompare);
+    //$scope.$apply();
+  }
+
+  $scope.remove = function(index) {
+    $scope.actors.splice(index,1);
+    $scope.$apply();
+  }
 
 });
--- app/js/directives/actors.js
+++ app/js/directives/actors.js
@@ -1,126 +1,137 @@
 /**
- * Player and Monster cards behaviour
- */
+* Player and Monster cards behaviour
+*/
 app.directive('actor',function($timeout){
-    return {
-        templateUrl: 'views/actor.html',
-        scope: {
-            actorType: '=type',
-            dropActor: '&dropped',
-            deleteActor: '&deleted',
-        },
-        link: function(scope,element,attr) {
+  return {
+    templateUrl: 'views/actor.html',
+    scope: {
+      actorType: '=type',
+      dropActor: '&dropped',
+      deleteActor: '&deleted',
+    },
+    link: function(scope,element,attr) {
 
-            var longpress;
-            var editingValue;
+      var longpress;
+      var editingValue;
+      var placeholder = true;
+      var draggedElement;
 
-            var placeholder = true;
-            var draggedElement;
-
-            function dragHandler(){
-                if(longpress) {
-                    element[0].draggable = true;
-                    element[0].contentEditable = false;
-                    angular.element(element[0]).addClass('dragging');
-                    editingValue = false;
-                }
-            }
-
-            element.on('mousedown',function(e){
-                longpress = true;
-                $timeout(dragHandler.bind(), 200);
-            });
-
-            element.on('mouseup',function(e){
-                if(longpress) {
-                    longpress = false;
-                    element[0].draggable = false;
-                    element[0].contentEditable = true;
-                }
-                angular.element(element[0]).removeClass('dragging');
-                if(e.target.nodeName === 'A') {
-                    scope.deleteActor({ index : attr.index});
-                }
-            });
-
-            element.on('mousemove',function(e){
-                longpress = false;
-            });
-
-
-
-
-
-
-            // Fired in the dragged element
-            element.on('dragstart', function(e){
-                e.dataTransfer.setData('text/plain', attr.index);
-                draggedElement = e.target;
-            });
-
-            // Fired in the element underneath
-            element.on('dragover',function(e){
-                e.preventDefault(); // prevent the auto adding in input field
-                if (placeholder &&
-                    element[0] !== draggedElement)
-                {
-                    angular.element(element[0]).addClass('actor--placeholder');
-                    placeholder = false;
-                }
-            });
-
-            // Fired in the element underneath
-            element.on('dragleave', function(e){
-                angular.element(e.target).removeClass('actor--placeholder');
-                placeholder = true;
-
-            });
-
-            // Fired in the element underneath
-            element.on('drop',function(e){
-                placeholder = true;
-                var initialValue = e.dataTransfer.getData('text/plain');
-                var targetValue = angular.element(this).scope().$index;
-                scope.dropActor( { initial : initialValue, target: targetValue });
-
-                angular.element(document.querySelectorAll('actor')).removeClass('dragging actor--placeholder');
-                dragCounter = 0;
-            });
-
-            // Fired on the dragged element
-            element.on('dragend', function(e){
-                draggedElement = null;
-            });
-
-
-
-
-
-            // Touch Still need fixing
-
-            // element.on('touchstart',function(e){
-            //     longpress = true;
-            //     e.preventDefault();
-            //     element[0].draggable = true;
-            //     editingValue = true;
-            //     $timeout(dragHandler.bind(), 200);
-            // });
-            //
-            // element.on('touchend',function(e){
-            //
-            //     if(editingValue === true) {
-            //         e.target.focus();
-            //     }
-            //     if(longpress) {
-            //         longpress = false;
-            //         element[0].draggable = false;
-            //         element[0].contentEditable = true;
-            //     }
-            //     angular.element(element[0]).removeClass('dragging');
-            //     if(e.target.nodeName === 'A') {
-            //         scope.deleteActor({ index : attr.index});
-            //     }
-            // });
+      function dragHandler(){
+        if(longpress) {
+          element[0].draggable = true;
+          element[0].contentEditable = false;
+          angular.element(element[0]).addClass('dragging');
+          editingValue = false;
         }
+      }
+
+
+
+
+      
+      /**
+      *  Desktop Events
+      */
+      element.on('mousedown',function(e){
+        longpress = true;
+        $timeout(dragHandler.bind(), 200);
+      });
+
+      element.on('mouseup',function(e){
+        if(longpress) {
+          longpress = false;
+          element[0].draggable = false;
+          element[0].contentEditable = true;
+        }
+        angular.element(element[0]).removeClass('dragging');
+        if(e.target.nodeName === 'A') {
+          scope.deleteActor({ index : attr.index});
+        }
+      });
+
+      element.on('mousemove',function(e){
+        longpress = false;
+      });
+
+
+
+
+
+      /**
+      *  Mouse Events
+      */
+      element.on('touchstart',function(e){
+        longpress = true;
+        e.preventDefault();
+        element[0].draggable = true;
+        editingValue = true;
+        $timeout(dragHandler.bind(), 200);
+      });
+
+      element.on('touchend',function(e){
+
+        if(editingValue === true) {
+          e.target.focus();
+        }
+        if(longpress) {
+          longpress = false;
+          element[0].draggable = false;
+          element[0].contentEditable = true;
+        }
+        angular.element(element[0]).removeClass('dragging');
+        if(e.target.nodeName === 'A') {
+          scope.deleteActor({ index : attr.index});
+        }
+      });
+
+
+
+
+      /**
+      *  Drag and Drop Events
+      */
+
+      // Fired in the dragged element
+      element.on('dragstart', function(e){
+        e.dataTransfer.setData('text/plain', attr.index);
+        draggedElement = e.target;
+      });
+
+      // Fired in the element underneath
+      element.on('dragover',function(e){
+        e.preventDefault(); // prevent the auto adding in input field
+        if (placeholder &&
+          element[0] !== draggedElement)
+          {
+            angular.element(document.querySelectorAll('actor')).removeClass('dragging actor--placeholder');
+            angular.element(element[0]).addClass('actor--placeholder');
+            placeholder = false;
+          }
+        });
+
+        // Fired in the element underneath
+        element.on('dragleave', function(e){
+          angular.element(e.target).removeClass('actor--placeholder');
+          placeholder = true;
+
+        });
+
+        // Fired in the element underneath
+        element.on('drop',function(e){
+          placeholder = true;
+          var initialValue = e.dataTransfer.getData('text/plain');
+          var targetValue = angular.element(this).scope().$index;
+          scope.dropActor( { initial : initialValue, target: targetValue });
+
+          angular.element(document.querySelectorAll('actor')).removeClass('dragging actor--placeholder');
+          dragCounter = 0;
+        });
+
+        // Fired on the dragged element
+        element.on('dragend', function(e){
+          draggedElement = null;
+          angular.element(document.querySelectorAll('actor')).removeClass('dragging actor--placeholder');
+        });
+      }
     }
-});
+  });
